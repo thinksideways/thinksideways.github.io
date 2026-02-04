9a50cb12250a2ecc49b1e7313cb053179ddb40ba
Sam Kaiser <samuel.k.public@gmail.com>
2026-02-04 17:17:51 -0600

    Assignment 2: Bootstrap toast based on data attributes and autohide disabled

--- discount.js
+++ discount.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
+    $(document).on('keydown', function(e) {
+        e.preventDefault();
+        if (e.key === "Escape") {
+            bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).hide();
+        }
+    });
+
     $('.discount-toast').click(function(e) {
         e.preventDefault();
-        console.log("clicked");
+
+        $('.toast-header strong').html(e.target.dataset['product']);
+        $('.toast-header small').html(e.target.dataset['discounttitle']);
+        $('.toast-body').html("Discount Code: " + e.target.dataset['discountcode']);
+
         bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
     });
 })
