d0440d6bd27943a085a0ab2536f86a0082b11c04
Sam Kaiser <samuel.k.public@gmail.com>
2026-02-05 18:33:09 -0600

    Balloons demo follow-along (pre homework state)

--- balloons.js
+++ balloons.js
@@ -6,4 +6,17 @@ $(document).ready(function() {
         autohide: true,
         format: 'MM-dd'
     });
+
+    // uncheck all boxes by default (Firefox)
+    document.querySelectorAll('.form-check-input').forEach(c => c.checked = false);
+
+    $('#checkbox-card').on('change', function(e) {
+        if (e.target.classList.contains('form-check-input')) {
+            document.getElementById(e.target.id + 'Img').style.visibility = e.target.checked ? "visible" : "hidden";
+            const elem = document.getElementById(e.target.id + 'Img');
+            elem.style.visibility = "visible";
+            elem.classList.remove("animate__animated", "animate__bounceInDown", "animate__bounceOutUp");
+            e.target.checked ? elem.classList.add("animate__animated", "animate__bounceInDown") : elem.classList.add("animate__animated", "animate__bounceOutUp");
+        }
+    });
 });
